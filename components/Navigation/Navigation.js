66e281f3d507a769efe5fac43420a9c0d117cec2
Francisco Marcos <fmarcos83@gmail.com>
2015-11-18 08:50:31 +0100

    initial commit

--- components/Navigation/Navigation.js
+++ components/Navigation/Navigation.js
@@ -6,18 +6,27 @@
 
 import React from 'react';
 import './Navigation.scss';
+import Tabs from "material-ui/lib/tabs/tabs";
+import Tab from "material-ui/lib/tabs/tab";
 import Link from '../Link';
 
 function Navigation() {
+  var tabs = [
+      {name:'HOME',link:'/'},
+      {name:'ABOUT',link:'/about'}
+  ];
+  var handleClick = function(){
+      window.Link = Link;
+  };
+  var tabValues = tabs.map(function(linkValue, key){
+    return (
+        <Tab href={linkValue.link} onClick={Link.handleClick} key={key} label={linkValue.name}></Tab>
+    );
+  });
   return (
-    <ul className="Navigation" role="menu">
-      <li className="Navigation-item">
-        <a className="Navigation-link" href="/" onClick={Link.handleClick}>Home</a>
-      </li>
-      <li className="Navigation-item">
-        <a className="Navigation-link" href="/about" onClick={Link.handleClick}>About</a>
-      </li>
-    </ul>
+      <Tabs>
+        {tabValues}
+      </Tabs>
   );
 }
 
