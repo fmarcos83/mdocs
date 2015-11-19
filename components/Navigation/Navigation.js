fe3ab8f50c77d4d8de06effa8fe7e060c197fa19
Francisco Marcos <fmarcos83@gmail.com>
2015-11-19 01:51:55 +0100

    header navigation and transition tabs working

--- components/Navigation/Navigation.js
+++ components/Navigation/Navigation.js
@@ -6,28 +6,57 @@
 
 import React from 'react';
 import './Navigation.scss';
-import Tabs from "material-ui/lib/tabs/tabs";
-import Tab from "material-ui/lib/tabs/tab";
-import Link from '../Link';
+import Tabs from 'material-ui/lib/tabs/tabs';
+import Tab from 'material-ui/lib/tabs/tab';
+import AppBar from 'material-ui/lib/app-bar';
+import FlatButton from 'material-ui/lib/flat-button';
+import Location from '../../core/Location';
 
-function Navigation() {
-  var tabs = [
-      {name:'HOME',link:'/'},
-      {name:'ABOUT',link:'/about'}
-  ];
-  var handleClick = function(){
-      window.Link = Link;
-  };
-  var tabValues = tabs.map(function(linkValue, key){
-    return (
-        <Tab href={linkValue.link} onClick={Link.handleClick} key={key} label={linkValue.name}></Tab>
-    );
-  });
-  return (
-      <Tabs>
-        {tabValues}
-      </Tabs>
-  );
+var slideIndex = "0"
+var handleClick = function(index, event){
+    let allowTransition = true;
+    let path='/'
+    event.preventDefault();
+    slideIndex = index
+    switch(index){
+        case "0":
+            path='/'
+        break;
+        case "1":
+            path='/start'
+        break;
+        case "2":
+            path='/guides'
+        break;
+        case "3":
+            path='/developer'
+        break;
+        default:
+            path='/'
+        break;
+    }
+    this.forceUpdate();
+    Location.pushState(null,path);
 }
-
+var Navigation = React.createClass({
+    render(){
+        return (
+              <AppBar
+                  showMenuIconButton={false}
+                  title={[<img key={0} src={require("./m_logo_white.svg")}></img>,
+                          <span key={1}>docs</span>]}
+                  iconElementRight={
+                      <div>
+                          <Tabs value={slideIndex}  tabItemContainerStyle={{height:'64px'}}>
+                            <Tab onClick={handleClick.bind(this,"0")} value="0" label="BIG PICTURE"></Tab>
+                            <Tab onClick={handleClick.bind(this,"1")} value="1" label="GETTING STARTED"></Tab>
+                            <Tab onClick={handleClick.bind(this,"2")} value="2" label="GUIDES"></Tab>
+                            <Tab onClick={handleClick.bind(this,"3")} value="3" label="DEVELOPER"></Tab>
+                          </Tabs>
+                      </div>
+                  }
+              />
+          );
+    }
+});
 export default Navigation;
