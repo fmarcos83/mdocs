997d9725375d0e9da4c0fe00cc2e3fd23f59bdb6
Francisco Marcos <fmarcos83@gmail.com>
2015-11-20 22:19:41 +0100

    fixed server side rendering on mdocs

--- app.js
+++ app.js
@@ -11,6 +11,12 @@ import { canUseDOM } from 'fbjs/lib/ExecutionEnvironment';
 import Location from './core/Location';
 import Layout from './components/Layout';
 
+if(!canUseDOM){
+    global.navigator = {
+        userAgent: 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/45.0.2454.101 Chrome/45.0.2454.101 Safari/537.36'
+    }
+}
+
 const routes = {}; // Auto-generated on build. See tools/lib/routes-loader.js
 
 const route = async (path, callback) => {
