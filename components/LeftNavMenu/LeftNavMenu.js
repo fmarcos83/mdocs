319d67cabb33bd4cb33ef90b18557efbe24ff97f
Francisco Marcos <fmarcos83@gmail.com>
2015-11-21 01:13:07 +0100

    changing to list items

--- components/LeftNavMenu/LeftNavMenu.js
+++ components/LeftNavMenu/LeftNavMenu.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import LeftNav from 'material-ui/lib/left-nav'
+import List from 'material-ui/lib/lists/list'
+import ListDivider from 'material-ui/lib/lists/list-divider'
+import ListItem from 'material-ui/lib/lists/list-item'
 
 let menuItems = [
     {route: 'intro', text: 'INTRO'},
@@ -9,10 +12,42 @@ let menuItems = [
     {route: 'sdks', text: 'SDKS'},
     {route: 'samples', text: 'SAMPLES'}
 ]
+var clickToggle = function(){
+    console.log('entra aqui');
+}
 var LeftNavMenu = React.createClass({
     render(){
         return (
-            <LeftNav style={{paddingTop:'67px'}} ref="leftNav" docked={true} menuItems={menuItems} />
+            <LeftNav style={{paddingTop:'64px'}} ref="leftNav" docked={true} >
+                <List>
+                    <ListItem autoGenerateNestedIndicator={false} nestedItems={[
+                        <ListItem key={0}>The Big Picture</ListItem>,
+                        <ListItem key={1}>Major Concepts</ListItem>,
+                        <ListItem key={2}>Get your own store</ListItem>
+                    ]}>
+                        INTRO
+                    </ListItem>
+                    <ListItem>
+                        THEME
+                    </ListItem>
+                    <ListItem>
+                        APP DEVELOPMENT
+                    </ListItem>
+                    <ListItem autoGenerateNestedIndicator={false} nestedItems={[
+                        <ListItem key={0} nestedItems={[
+                            <ListItem key={0}>Commerce</ListItem>
+                        ]}></ListItem>
+                    ]}>
+                        REST API REFERENCE
+                    </ListItem>
+                    <ListItem>
+                        SDKS
+                    </ListItem>
+                    <ListItem>
+                        SAMPLES
+                    </ListItem>
+                </List>
+            </LeftNav>
         )
     }
 })
