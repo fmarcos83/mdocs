3757d3609034172d57597d202fa465953456e784
Francisco Marcos <fmarcos83@gmail.com>
2015-11-22 15:44:05 +0100

    side nav menu working fine

--- components/LeftNavMenu/LeftNavMenu.js
+++ components/LeftNavMenu/LeftNavMenu.js
@@ -7,26 +7,74 @@ import {Accordion, AccordionItem}from 'react-sanfona'
 import Ink from 'react-ink';
 import './LeftNavMenu.scss'
 
-let menuItems = [
-    {route: 'intro', text: 'INTRO'},
-    {route: 'theme', text: 'THEME'},
-    {route: 'app_dev', text: 'APP DEVELOPMENT'},
-    {route: 'rest_api', text: 'REST API REFERENCE'},
-    {route: 'sdks', text: 'SDKS'},
-    {route: 'samples', text: 'SAMPLES'}
-]
-var LeftNavMenu = React.createClass({
+let LeftNavMenu = React.createClass({
     render(){
         return (
-            <LeftNav style={{paddingTop:'72px',overflow: 'auto', background:'rgba(0,0,0,0.05)'}} ref="leftNav" docked={true} >
+            <LeftNav style={{paddingTop:'72px',overflowY: 'auto', background:"rgba(0,0,0,0.05)"}} ref="leftNav" docked={true} >
                 <Accordion>
-                    {menuItems.map((item, key)=>{
-                        return(
-                            <AccordionItem  title={<div className="m-item-container"><Ink />{item.text}</div>} key={key}>
-                                HOLA K ASE
-                            </AccordionItem>
-                        );
-                    })}
+                    <AccordionItem  title={<div className="m-item-container"><Ink />INTRO</div>}>
+                        <ul>
+                            <li><a><span>The Big Picture</span></a></li>
+                            <li><a><span>Major Concepts</span></a></li>
+                            <li><a><span>Get your own store</span></a></li>
+                        </ul>
+                    </AccordionItem>
+                    <AccordionItem  title={<div className="m-item-container"><Ink />THEME</div>}>
+                        <ul>
+                            <li><a><span>Build a theme</span></a></li>
+                            <li><a><span>Build a widget</span></a></li>
+                            <li><a><span>Looping through records</span></a></li>
+                            <li><a><span>Deploying your app</span></a></li>
+                            <li><a><span>Inheritence</span></a></li>
+                            <li><a><span>Hypr</span></a></li>
+                            <li><a><span>Making it fast</span></a></li>
+                            <li><a><span>Optimizing for seo</span></a></li>
+                        </ul>
+                    </AccordionItem>
+                    <AccordionItem  title={<div className="m-item-container"><Ink />APP DEVELOPMENT</div>}>
+                        <ul>
+                            <li><a><span>Intro to apps</span></a></li>
+                            <li><a><span>Build an app</span></a></li>
+                            <li><a><span>Webhooks</span></a></li>
+                            <li><a><span>Configuration UI</span></a></li>
+                            <li><a><span>Authentication</span></a></li>
+                            <li><a><span>Arc.js</span></a></li>
+                        </ul>
+                    </AccordionItem>
+                    <AccordionItem  title={<div className="m-item-container"><Ink />REST API REFERENCE</div>}>
+                        <ul>
+                            <li>
+                                <a><span className="m-active">Commerce</span></a>
+                                <ul>
+                                    <li>
+                                        <a><span>admin</span></a>
+                                        <ul>
+                                            <li>
+                                                <a><span className="m-active">carts</span></a>
+                                                <ul>
+                                                    <li><a><span>cartCoupons</span></a></li>
+                                                    <li><a><span>cartItems</span></a></li>
+                                                    <li>
+                                                        <a><span className="m-active">cartExtendedProperties</span></a>
+                                                        <ul>
+                                                            <li><a><span>cartTax</span></a></li>
+                                                            <li><a><span>cartShipping</span></a></li>
+                                                            <li><a><span>cartGiftWrapping</span></a></li>
+                                                            <li><a><span>cartDelivery</span></a></li>
+                                                        </ul>
+                                                    </li>
+                                                </ul>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </AccordionItem>
+                    <AccordionItem  title={<div className="m-item-container"><Ink />SDKS</div>}>
+                    </AccordionItem>
+                    <AccordionItem  title={<div className="m-item-container"><Ink />SAMPLES</div>}>
+                    </AccordionItem>
                 </Accordion>
             </LeftNav>
         )
